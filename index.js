90941812afc1bfda95209ba8270ad3d62796a582
Hubert <dennishubert69420@gmail.com>
2025-10-26 16:21:53 +0700

    ahhh

--- index.js
+++ index.js
@@ -136,17 +136,17 @@ app.post('/register', upload.single('profile'), async (req, res) => {
 ---------------------------------- */
 app.post('/login', async (req, res) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) return res.status(400).json({ message: 'Email & password required' });
+    const { name, password } = req.body;
+    if (!name || !password) return res.status(400).json({ message: 'name & password required' });
 
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM users WHERE name = $1', [name]);
     if (result.rows.length === 0) return res.status(400).json({ message: 'User not found' });
 
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
 
-    const token = jwt.sign({ id: user.id, name: user.name, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ id: user.id, name: user.name, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
     res.cookie('token', token, {
       httpOnly: false,
